1dba9b5b06669a4a5732ed377124f1504ae7463d
Milo <mil.swiatek1@gmail.com>
2024-04-27 10:20:06 +0000

    first controller and service for ads

---
+++ app/tests/Controller/AdControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Ad List Controller Test.
+ */
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class AdListControllerTest.
+ */
+class AdControllerTest extends WebTestCase
+{
+    /**
+     * Test Ad List Route.
+     */
+    public function testAdRoute(): void
+    {
+        // given
+
+        $client = static::createClient();
+
+        // when
+
+        $client->request('GET', '/adlist');
+        $responseCode = $client->getResponse()->getStatusCode();
+
+        // then
+
+        $this->assertEquals(200, $responseCode);
+    }
+}
